2a561910c256952bc539aa1a2fb54771e91b4eb1
DarthSim <darthsim@gmail.com>
2022-01-31 18:01:54 +0600

    Add manifest tagged as latest

--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
+
 TAG_NAME=${1:-latest}
-IMAGE_NAME="darthsim/imgproxy-base:$TAG_NAME"
+REPO_NAME="darthsim/imgproxy-base"
+
+IMAGE_NAME="$REPO_NAME:$TAG_NAME"
+LATEST_IMAGE_NAME="$REPO_NAME:latest"
 
 echo "Image name: $IMAGE_NAME"
 
@@ -11,7 +15,12 @@ export DOCKER_CLI_EXPERIMENTAL=enabled
 docker push $IMAGE_NAME-amd64
 docker push $IMAGE_NAME-arm64
 
-docker manifest create $IMAGE_NAME -a $IMAGE_NAME-amd64 -a $IMAGE_NAME-arm64
-docker manifest annotate $IMAGE_NAME $IMAGE_NAME-amd64 --arch amd64
-docker manifest annotate $IMAGE_NAME $IMAGE_NAME-arm64 --arch arm64 --variant v8
-docker manifest push $IMAGE_NAME
+push_manifest() {
+  docker manifest create $1 -a $2-amd64 -a $2-arm64
+  docker manifest annotate $1 $2-amd64 --arch amd64
+  docker manifest annotate $1 $2-arm64 --arch arm64 --variant v8
+  docker manifest push $1
+}
+
+push_manifest $IMAGE_NAME $IMAGE_NAME
+push_manifest $LATEST_IMAGE_NAME $IMAGE_NAME
